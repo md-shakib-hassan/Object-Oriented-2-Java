375053717b5529a14c36b74e312824c49122810a
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-02-25 23:34:04 +0600

    adding 2nd problem of which is given by NN teacher

---
+++ Exception Handling/StringToNumberDivision.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class StringToNumberDivision {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter first value (as string): ");
+            String s1 = sc.next();
+            System.out.print("Enter second value (as string): ");
+            String s2 = sc.next();
+
+            int a = Integer.parseInt(s1);   // may throw NumberFormatException
+            int b = Integer.parseInt(s2);
+
+            int result = a / b;             // may throw ArithmeticException
+            System.out.println("Result: " + result);
+
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
