a5ccd1a9ed11365607726e535b3631f94adb20e7
md-shakib-hassan <arefinshakibkhan@gmail.com>
2026-02-25 20:46:22 +0600

    finally

--- Exception Handling/Division_Array_Access.java
+++ Exception Handling/Division_Array_Access.java
@@ -1,45 +1,38 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Division_Array_Access {
     public static void main(String[] args) {
-        int num1, num2;
-        System.out.println("Input Two integer: ");
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        num1 = input.nextInt();
-        System.out.println("Enter second number: ");
-        num2 = input.nextInt();
 
-        int result = num1/num2;
 
-        int arr[] = new int [3];
+        try {
+            int num1, num2;
+            System.out.println("Input Two integer. ");
+            Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter index where result will be store:");
-        int index = input.nextInt();
+            System.out.print("Enter first number: ");
+            num1 = input.nextInt();
+            System.out.print("Enter second number: ");
+            num2 = input.nextInt();
 
-        try{
-            try{
-               int divion = result/0;
-                System.out.println("that number divide.");
-            }catch (Exception e){
-                System.out.println("Exception: "+e);
+            int result = num1 / num2;
 
-            }
-            try {
-                System.out.println("index is : "+arr[index]);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-            try {
-                System.out.println("invalid input.");
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-        }catch(Exception e ){
+            int arr[] = new int[3];
+
+            System.out.println("Enter index where result will be store:");
+            int index = input.nextInt();
+
+            arr[index] = result;
+            int divison = result/0;
+            System.out.println("divison successful.");
+
+        } catch (ArithmeticException e) {
+           System.out.println(e);
+        }catch(ArrayIndexOutOfBoundsException e){
             System.out.println(e);
+        }catch(InputMismatchException e){
+                System.out.println(e);
         }
-
-
 
     }
 }
